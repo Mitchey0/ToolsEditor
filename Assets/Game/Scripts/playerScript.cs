5f4d6ccd3dd41c5ecb27a5c316e1f151f2d875c1
MichelleO <Ortizmich87@gmail.com>
2024-05-06 13:18:54 -0500

    Player Setup
    Added a simple player and movement controller

---
+++ Assets/Game/Scripts/playerScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    private CharacterController playerController;
+
+    public float Speed = 5f;
+
+    void Start()
+    {
+        playerController = GetComponent<CharacterController>();
+    }
+
+ 
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        playerController.Move(move * Time.deltaTime * Speed);
+    }
+}
