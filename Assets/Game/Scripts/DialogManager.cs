18b08311b0b276364c7cb6f0633afbcc4ec3272a
MichelleO <Ortizmich87@gmail.com>
2024-05-10 00:40:01 -0500

    Dialogue Trigger and Manager
    Inputted scripts for trigger and manager, customizing to fit the current scene.

--- Assets/Game/Scripts/DialogManager.cs
+++ Assets/Game/Scripts/DialogManager.cs
@@ -1,18 +1,128 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using TMPro;
 
 public class DialogManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject dialogueParent;
+    [SerializeField] private TMP_Text dialogueText;
+    [SerializeField] private Button option1Button;
+    [SerializeField] private Button option2Button;
+
+    [SerializeField] private float typingSpeed = 0.05f;
+
+    private List<dialogueString> dialogueList;
+
+    [Header("Player")]
+    [SerializeField] private playerScript firstPersonController;
+    private int currentDialogueIndex = 0;
+
+    private void Start()
     {
-        
+        dialogueParent.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DialogueStart(List<dialogueString> textToPrint, Transform NPC)
     {
-        
+        dialogueParent.SetActive(true);
+        firstPersonController.enabled = false;
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        dialogueList = textToPrint;
+        currentDialogueIndex = 0;
+
+        DisableButtons();
+
+        StartCoroutine(PrintDialogue());
+    }
+
+    private void DisableButtons()
+    {
+        option1Button.interactable = false;
+        option2Button.interactable = false;
+
+        option1Button.GetComponentInChildren<TMP_Text>().text = "No Option";
+        option2Button.GetComponentInChildren<TMP_Text>().text = "No Option";
+    }
+
+    private bool optionSelected = false;
+
+    private IEnumerator PrintDialogue()
+    {
+        while (currentDialogueIndex < dialogueList.Count)
+        {
+            dialogueString line = dialogueList[currentDialogueIndex];
+            
+            line.startDialogueEvent?.Invoke();
+
+            if (line.isQuestion)
+            {
+                yield return StartCoroutine(TypeText(line.text));
+
+                option1Button.interactable = true;
+                option2Button.interactable = true;
+
+                option1Button.GetComponentInChildren<TMP_Text>().text = line.answerOption1;
+                option2Button.GetComponentInChildren<TMP_Text>().text = line.answerOption2;
+
+                option1Button.onClick.AddListener(() => HandleOptionSelected(line.option1IndexJump));
+                option2Button.onClick.AddListener(() => HandleOptionSelected(line.option2IndexJump));
+
+                yield return new WaitUntil(() => optionSelected);
+            }
+            else
+            {
+                yield return StartCoroutine(TypeText(line.text));
+            }
+
+            line.endDialogueEvent?.Invoke();
+
+            optionSelected = false;
+        }
+
+        DialogueStop();
+    }
+
+    private void HandleOptionSelected(int indexJump)
+    {
+        optionSelected = true;
+        DisableButtons();
+
+        currentDialogueIndex = indexJump;
+    }
+
+    private IEnumerator TypeText(string text)
+    {
+        dialogueText.text = "";
+        foreach (char letter in text.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+        if (!dialogueList[currentDialogueIndex].isQuestion)
+        {
+            yield return new WaitUntil(() => Input.GetMouseButton(0));
+        }
+        if (dialogueList[currentDialogueIndex].isEnd)
+            DialogueStop();
+
+        currentDialogueIndex++;
+    }
+
+    private void DialogueStop()
+    {
+        StopAllCoroutines();
+        dialogueText.text = "";
+        dialogueParent.SetActive(false);
+
+        firstPersonController.enabled = true;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 }
